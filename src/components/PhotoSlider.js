afcd503504698b653644239f41415ed95d77b295
kmcandersen <kristen.andersen@gmail.com>
2021-05-24 22:43:27 -0500

    enlarge PhotoSlider image in Modal

--- src/components/PhotoSlider.js
+++ src/components/PhotoSlider.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
 import {
+  Button,
   Dimensions,
   Image,
+  ImageBackground,
+  Modal,
   ScrollView,
   StyleSheet,
   Text,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
+import Screen from './Screen';
 
 const { width } = Dimensions.get('window');
-const height = width * 0.6;
+const thumbHeight = width * 0.6;
 
 const PhotoSlider = ({ photos }) => {
   const [activePhoto, setActivePhoto] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const changePhoto = ({ nativeEvent }) => {
     const slide = Math.ceil(
@@ -34,7 +40,23 @@ const PhotoSlider = ({ photos }) => {
         style={styles.container}
       >
         {photos.map((photo, index) => (
-          <Image key={index} source={{ uri: photo }} style={styles.photo} />
+          <View key={index}>
+            <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+              <Image source={{ uri: photo }} style={styles.photo} />
+            </TouchableWithoutFeedback>
+            <Modal visible={modalVisible} animationType='slide'>
+              <Screen>
+                <Button title='Close' onPress={() => setModalVisible(false)} />
+                <View style={styles.photoEnlargedContainer}>
+                  <ImageBackground
+                    source={{ uri: photos[activePhoto] }}
+                    style={styles.photoEnlarged}
+                    //resizeMode={'center'}
+                  ></ImageBackground>
+                </View>
+              </Screen>
+            </Modal>
+          </View>
         ))}
       </ScrollView>
       <View style={styles.dotContainer}>
@@ -54,7 +76,7 @@ const PhotoSlider = ({ photos }) => {
 const styles = StyleSheet.create({
   container: {
     width,
-    height,
+    height: thumbHeight,
   },
   dot: {
     fontSize: width / 10,
@@ -74,8 +96,20 @@ const styles = StyleSheet.create({
   },
   photo: {
     width,
-    height,
+    height: thumbHeight,
     resizeMode: 'cover',
+  },
+  photoEnlarged: {
+    height: width,
+    // aspect ratio = 4:3
+    width: width * 1.33,
+    transform: [{ rotate: '90deg' }],
+  },
+  photoEnlargedContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
